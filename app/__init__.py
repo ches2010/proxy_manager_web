74725cbee859508f302eb9be8c39bd27e542a55a
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-09-18 15:10:33 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
-# 使 app 目录成为一个 Python 包
+# app/__init__.py
+from flask import Flask
 
+def create_app():
+    app = Flask(__name__)
+    # 可加载配置
+    # app.config.from_json("../config/config.json")
+    return app
 
-
+# 供 launch.py 调用
+app = create_app()
